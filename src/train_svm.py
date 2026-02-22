1f52011275ed3922b816985b9de08714e49fd126
samriddhi2810 <144598116+samriddhi2810@users.noreply.github.com>
2026-02-22 23:16:28 +0530

    Added evaluation plots and updated SVM training script

--- src/train_svm.py
+++ src/train_svm.py
@@ -1,8 +1,14 @@
+import matplotlib
+matplotlib.use("Agg")
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load cleaned data
 df = pd.read_csv("data/cleaned_requirements.csv")
@@ -10,22 +16,72 @@ df = pd.read_csv("data/cleaned_requirements.csv")
 X = df["clean_text"]
 y = df["label"]
 
-# Split data
+# Stratified Split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=42
+    X,
+    y,
+    test_size=0.25,
+    random_state=42,
+    stratify=y
 )
 
-# Convert text to numbers (TF-IDF)
-vectorizer = TfidfVectorizer()
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
+# 🔹 PIPELINE (TF-IDF + SVM together)
+pipeline = Pipeline([
+    ("tfidf", TfidfVectorizer(
+        ngram_range=(1, 2),   # unigrams + bigrams
+        max_df=0.9,
+        min_df=2
+    )),
+    ("clf", LinearSVC(class_weight="balanced"))
+])
 
-# Train SVM
-model = LinearSVC(class_weight="balanced")
-model.fit(X_train_tfidf, y_train)
+# Train
+pipeline.fit(X_train, y_train)
 
 # Predict
-y_pred = model.predict(X_test_tfidf)
+y_pred = pipeline.predict(X_test)
 
-# Evaluate
+# Confusion Matrix
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+# Classification Report
+print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
+
+#Heapmap of Confusion Matrix
+cm = confusion_matrix(y_test, y_pred, labels=["functional", "non-functional"])
+
+plt.figure(figsize=(6,5))
+sns.heatmap(cm,
+            annot=True,
+            fmt="d",
+            cmap="rocket",
+            xticklabels=["Functional", "Non-Functional"],
+            yticklabels=["Functional", "Non-Functional"])
+
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix (SVM + TF-IDF Bigrams)")
+plt.tight_layout()
+plt.savefig("confusion_matrix.png")
+plt.close()
+
+# F1 bar chart
+report = classification_report(y_test, y_pred, output_dict=True)
+
+labels = ["Functional", "Non-Functional"]
+f1_scores = [
+    report["functional"]["f1-score"],
+    report["non-functional"]["f1-score"]
+]
+
+plt.figure(figsize=(6,5))
+plt.bar(labels, f1_scores,
+        color=["#2E004F", "#FF4D6D"])
+plt.ylim(0, 1)
+plt.ylabel("F1 Score")
+plt.title("F1 Score per Class")
+plt.tight_layout()
+plt.savefig("f1_scores.png")
+plt.close()
